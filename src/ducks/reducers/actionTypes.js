d1593323e4326b3661eaccf8090a7a935aab003e
Wesley Walker <wesley0626@msn.com>
2019-08-19 17:52:32 -0600

    name apearing

--- src/ducks/reducers/actionTypes.js
+++ src/ducks/reducers/actionTypes.js
@@ -1,1 +1,5 @@
 export const GET_POTENTIAL_MATCHES = "GET_POTENTIAL_MATCHES"
+export const GET_USER = 'GET_USER';
+export const REGISTER = 'REGISTER';
+export const LOGIN = 'LOGIN';
+export const LOGOUT = 'LOGOUT';
