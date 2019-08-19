a557a814c9ce25b62726a8cd39b8057524feff49
Krissy Hales <kristinakfriel@gmail.com>
2019-08-19 12:19:37 -0600

    auth completed

--- server/index.js
+++ server/index.js
@@ -1,17 +1,33 @@
 require("dotenv").config()
-const express = require('express')
-const massive = require('massive')
-const userController = require('./controllers/userController')
+const express = require('express');
+const session = require('express-session');
+const massive = require('massive');
+const userController = require('./controllers/userController');
+const sessionController = require('./controllers/sessionController');
+const authmw = require('./middleware/authCheck');
 const {SERVER_PORT, SESSION_SECRET, CONNECTION_STRING} = process.env
 
 const app = express()
+app.use(express.json())
+
+app.use(
+    session({
+        secret: SESSION_SECRET,
+        saveUninitialized: true,
+        resave: false
+    })
+)
 
 massive(CONNECTION_STRING).then(db => app.set('db', db)).catch(err => console.log('err', err))
 
-app.use(express.json())
-
-
 app.get(`/api/users/potential`, userController.getPotentialMatches)
+
+
+//session endpoints 
+app.post('/api/login', sessionController.login);
+app.post('/api/register', sessionController.register);
+app.delete('/api/logout', sessionController.logout);
+app.get('/api/user', authmw, sessionController.getUser);
 
 
 app.listen(SERVER_PORT, () => console.log(`Server listening on ${SERVER_PORT}`))
--- src/ducks/reducers/actionTypes.js
+++ src/ducks/reducers/actionTypes.js
@@ -1,1 +1,8 @@
 export const GET_POTENTIAL_MATCHES = "GET_POTENTIAL_MATCHES"
+
+
+export const LOGIN = 'LOGIN';
+export const REGISTER = 'REGISTER';
+export const LOGOUT = 'LOGOUT';
+export const GET_USER = 'GET_USER';
+
