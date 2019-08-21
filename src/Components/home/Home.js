9a6fba2b27b5a3f8b69e309c50087bd2901c16cf
KhiavDim Lee <khiavdim@KhiavDims-MacBook-Pro.local>
2019-08-21 11:29:06 -0600

    No changes

--- src/Components/home/Home.js
+++ src/Components/home/Home.js
@@ -1,178 +1,193 @@
-import React, {Component} from 'react'
-import {connect} from 'react-redux'
-import {getPotentialMatches} from '../../ducks/reducers/userReducer'
-import {swipeLeft, swipeRight} from "../../ducks/reducers/swipeReducer"
-import Swipe from 'react-easy-swipe'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { getPotentialMatches } from "../../ducks/reducers/userReducer";
+import { swipeLeft, swipeRight } from "../../ducks/reducers/swipeReducer";
+import Swipe from "react-easy-swipe";
+import "./Home.css";
 
-class Home extends Component{
-  constructor(){
-    super()
+class Home extends Component {
+  constructor() {
+    super();
     this.state = {
-      matchesWithCompatability:[],
+      matchesWithCompatability: [],
       counter: 0
-    }
-  }
-  
-  async componentDidMount(){
-    let {getPotentialMatches} = this.props
-    await getPotentialMatches()
-    this.setCompatability(this.props.potentialMatches)    
-  }
-  
-  onSwipeStart(event){
-    console.log("Start Swiping...", event)
+    };
   }
 
-  onSwipeMove(position, event){
-    console.log(`Moved ${position.x} pizels horizontally`, event)
-    console.log(`Moved ${position.y} pizels vertically`, event)
+  async componentDidMount() {
+    let { getPotentialMatches } = this.props;
+    await getPotentialMatches();
+    this.setCompatability(this.props.potentialMatches);
   }
 
-  onSwipeEnd(event){
-    console.log("End Swiping...", event)
+  onSwipeStart(event) {
+    console.log("Start Swiping...", event);
   }
 
-  setCompatability = (arr) => {
-    for(let i = 0; i < arr.length; i++){
+  onSwipeMove(position, event) {
+    console.log(`Moved ${position.x} pizels horizontally`, event);
+    console.log(`Moved ${position.y} pizels vertically`, event);
+  }
+
+  onSwipeEnd(event) {
+    console.log("End Swiping...", event);
+  }
+
+  setCompatability = arr => {
+    for (let i = 0; i < arr.length; i++) {
       let user1 = {
         user_id: 12,
-          user_appearance_id: 12,
-          name: "Kristina",
-          email: 2,
-          password: 2,
-          user_details_id: 12,
-          religion: "mormon",
-          ethnicity: "white",
-          description: "my name is kristina",
-          gender: "female",
-          intro_extro: 6,
-          user_interests_id: 12,
-          sports: true,
-          arts: false,
-          music: true,
-          books: false,
-          movies: true,
-          outdoors: false,
-          food: false,
-          pets: true,
-          netflix: true,
-          traveling: true,
-          tech: true,
-          fashion: false,
-          fitness: true,
-          gaming: false,
-          politics: true,
-          hair_color: "brown",
-          image1: "https://tse2.mm.bing.net/th?id=OIP.gkbT_J7jAwS-6zUxEdP7wAHaFY&pid=Api&P=0&w=224&h=163",
-          image2: 1,
-          image3: 1,
-          age: 25,
-          age_min: 18,
-          age_max: 100,
-          min_height: 55,
-          max_height: 60,
-          hair_color_pref: "brown",
-          user_details_pref: 12,
-          gender_pref: "male",
-          religion_pref: "mormon",
-          ethnicity_pref: "white",
-          intro_extro_pref: 5
-        }
-        
-        let user2 = arr[i]
-        console.log('user', user2)
-        let compatabilityCounter = 0
-        if(user1.ethnicity_pref === user2.ethnicity){
-          compatabilityCounter +=1
-        }
-        if(user1.hair_color_pref === user2.ethnicity){
-          compatabilityCounter +=1
-        }
-        if(user1.religion_pref === user2.religion){
-          compatabilityCounter +=1
-        }
-        if(user1.arts === user2.arts){
-          compatabilityCounter +=1
-        }
-        if(user1.books === user2.books){
-          compatabilityCounter +=1
-        }
-        if(user1.fashion === user2.fashion){
-          compatabilityCounter +=1
-        }
-        if(user1.fitness === user2.fitness){
-          compatabilityCounter +=1
-        }
-        if(user1.food === user2.food){
-          compatabilityCounter +=1
-        }
-        if(user1.gaming === user2.gaming){
-          compatabilityCounter +=1
-        }
-        if(user1.movies === user2.movies){
-          compatabilityCounter +=1
-        }
-        if(user1.music === user2.music){
-          compatabilityCounter +=1
-        }
-        if(user1.netflix === user2.netflix){
-          compatabilityCounter +=1
-        }
-        if(user1.outdoors === user2.outdoors){
-          compatabilityCounter +=1
-        }
-        if(user1.pets === user2.pets){
-          compatabilityCounter +=1
-        }
-        if(user1.politics === user2.politics){
-          compatabilityCounter +=1
-        }
-        let currentUser = {...arr[i], compatability: compatabilityCounter}
-        this.setState({matchesWithCompatability: [...this.state.matchesWithCompatability, currentUser]})
-    }
-  }
+        user_appearance_id: 12,
+        name: "Kristina",
+        email: 2,
+        password: 2,
+        user_details_id: 12,
+        religion: "mormon",
+        ethnicity: "white",
+        description: "my name is kristina",
+        gender: "female",
+        intro_extro: 6,
+        user_interests_id: 12,
+        sports: true,
+        arts: false,
+        music: true,
+        books: false,
+        movies: true,
+        outdoors: false,
+        food: false,
+        pets: true,
+        netflix: true,
+        traveling: true,
+        tech: true,
+        fashion: false,
+        fitness: true,
+        gaming: false,
+        politics: true,
+        hair_color: "brown",
+        image1:
+          "https://tse2.mm.bing.net/th?id=OIP.gkbT_J7jAwS-6zUxEdP7wAHaFY&pid=Api&P=0&w=224&h=163",
+        image2: 1,
+        image3: 1,
+        age: 25,
+        age_min: 18,
+        age_max: 100,
+        min_height: 55,
+        max_height: 60,
+        hair_color_pref: "brown",
+        user_details_pref: 12,
+        gender_pref: "male",
+        religion_pref: "mormon",
+        ethnicity_pref: "white",
+        intro_extro_pref: 5
+      };
 
-   actionSwipeLeft = () => {
-      console.log('left')
+      let user2 = arr[i];
+      console.log("user", user2);
+      let compatabilityCounter = 0;
+      if (user1.ethnicity_pref === user2.ethnicity) {
+        compatabilityCounter += 1;
+      }
+      if (user1.hair_color_pref === user2.ethnicity) {
+        compatabilityCounter += 1;
+      }
+      if (user1.religion_pref === user2.religion) {
+        compatabilityCounter += 1;
+      }
+      if (user1.arts === user2.arts) {
+        compatabilityCounter += 1;
+      }
+      if (user1.books === user2.books) {
+        compatabilityCounter += 1;
+      }
+      if (user1.fashion === user2.fashion) {
+        compatabilityCounter += 1;
+      }
+      if (user1.fitness === user2.fitness) {
+        compatabilityCounter += 1;
+      }
+      if (user1.food === user2.food) {
+        compatabilityCounter += 1;
+      }
+      if (user1.gaming === user2.gaming) {
+        compatabilityCounter += 1;
+      }
+      if (user1.movies === user2.movies) {
+        compatabilityCounter += 1;
+      }
+      if (user1.music === user2.music) {
+        compatabilityCounter += 1;
+      }
+      if (user1.netflix === user2.netflix) {
+        compatabilityCounter += 1;
+      }
+      if (user1.outdoors === user2.outdoors) {
+        compatabilityCounter += 1;
+      }
+      if (user1.pets === user2.pets) {
+        compatabilityCounter += 1;
+      }
+      if (user1.politics === user2.politics) {
+        compatabilityCounter += 1;
+      }
+      let currentUser = { ...arr[i], compatability: compatabilityCounter };
+      this.setState({
+        matchesWithCompatability: [
+          ...this.state.matchesWithCompatability,
+          currentUser
+        ]
+      });
     }
-      
-    
+  };
+  actionSwipeLeft = () => {
+    console.log("left");
+  };
 
-    actionSwipeRight= (id) => {
-       let {swipeRight} = this.props
-         console.log('right')
-         swipeRight(id)
-         this.setState({counter: this.state.counter+= 1})
-       }
-    
-    render(){
-      const compatable = this.state.matchesWithCompatability.sort((a,b) => (a.compatability< b.compatability) ? 1 : -1)
-      console.log('this', compatable)
-    return(
+  actionSwipeRight = id => {
+    let { swipeRight } = this.props;
+    console.log("right");
+    swipeRight(id);
+    this.setState({ counter: (this.state.counter += 1) });
+  };
+
+  render() {
+    const compatable = this.state.matchesWithCompatability.sort((a, b) =>
+      a.compatability < b.compatability ? 1 : -1
+    );
+    console.log("this", compatable);
+    return (
       <div>
         {compatable.length ? (
           <div>
-            {compatable.slice(0, 1).map(profile => 
-              <Swipe key={`swipeId-${profile.user_id}`} onSwipeLeft={() => this.actionSwipeLeft(profile.user_id)} onSwipeRight={() =>{ this.actionSwipeRight(profile.user_id)
-              compatable.splice(0, 1)} }>
+            {compatable.slice(0, 1).map(profile => (
+              <Swipe
+                key={`swipeId-${profile.user_id}`}
+                onSwipeLeft={() => this.actionSwipeLeft(profile.user_id)}
+                onSwipeRight={() => {
+                  this.actionSwipeRight(profile.user_id);
+                  compatable.splice(0, 1);
+                }}
+              >
                 {profile.name}
                 {profile.age}
-                <img src={profile.image1}/>
+                <img src={profile.image1} />
               </Swipe>
-              )}
+            ))}
           </div>
-        ):(
+        ) : (
           <div>Loading...</div>
         )}
       </div>
-      )
-    }
-  }
-    
-    function mapStateToProps(state){
-  return{
-    ...state.user
+    );
   }
 }
 
-export default connect(mapStateToProps, {getPotentialMatches, swipeLeft, swipeRight})(Home)
+function mapStateToProps(state) {
+  return {
+    ...state.user
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  { getPotentialMatches, swipeLeft, swipeRight }
+)(Home);
--- src/Components/register/About2.js
+++ src/Components/register/About2.js
@@ -1,17 +1,39 @@
 import React, { Component } from 'react';
 import './about.css';
-
+import {connect} from 'react-redux';
+import {addUserDetailsAndInterests} from './../../ducks/reducers/formReducer';
+import {Link} from 'react-router-dom';
 class About2 extends Component {
     constructor() {
         super()
         this.state = {
             religions: ['Agnostic', 'Atheist', 'Buddhism', 'Christians', 'Judaism', 'Islam', 'Spiritual', 'Taoism', 'Other'],
-            ethnicity: ['African American', 'Asian', 'Hispanic', 'Native American', 'Pacific Islander', 'White', 'Other'],
-            interest : ['Arts', 'Books', 'Outdoors', 'Fitness', 'Music', 'Movies', 'Food', 'Pets', 'Netflix', 'Traveling', 'Politics', 'Tech', 'Fashion', 'Gaming'],
-            display: false
+            races: ['African American', 'Asian', 'Hispanic', 'Native American', 'Pacific Islander', 'White', 'Other'],
+            interest: ['Arts', 'Books', 'Outdoors', 'Fitness', 'Music', 'Movies', 'Food', 'Pets', 'Netflix', 'Traveling', 'Politics', 'Tech', 'Fashion', 'Gaming'],
+            display: false,
+            gender: '',
+            religion: '',
+            ethnicity: '',
+            intro_extro: 0,
+            sports: false,
+            arts: false,
+            music: false,
+            books: false,
+            movies: false,
+            outdoors: false,
+            food: false,
+            pets: false,
+            netflix: false,
+            traveling: false,
+            tech: false,
+            fashion: false,
+            fitness: false,
+            gaming: false,
+            politics: false, 
+            description: '',
+            selected: false,
         }
     }
-
     flipInterestDisplay = () => {
         this.setState({
             display: !this.state.display
@@ -20,56 +42,64 @@ class About2 extends Component {
     
     female = () => {
         this.setState({
-            selected: !this.state.femaleSelected
+            gender: 'female'
         })
     }
-
     male = () => {
         this.setState({
-            selected: !this.state.maleSelected
+            gender: 'male'
+        })
+    }
+    handleChange = e => {
+        this.setState({
+            [e.target.name]: e.target.value
         })
     }
 
+    handleCheck = e => {
+        this.setState({
+            [e.target.name]: true
+        })
+    }
 
+    checked = () => {
+    }
+    handleSubmit = () => {
+        let {gender, religion, ethnicity, intro_extro, description, sports, arts, music, books, movies, outdoors, food, pets, netflix, traveling, tech, fashion, fitness, gaming, politics} = this.state
+        this.props.addUserDetailsAndInterests(gender, religion, ethnicity, intro_extro, description, sports, arts, music,books, movies, outdoors, food, pets, netflix, traveling, tech, fashion, fitness, gaming, politics)
+    }
     render() {
-        let {religions, ethnicity, display, interest} = this.state
+        let {religions, races, display, interest, gender, description, intro_extro} = this.state
         return (
             <div>
                 <h1 className='about_you_header'>More About You</h1>
                 
                 <div className='form_container'>
-
                     <div className='gender_container'>
                         <label className='form_labels' id='gender'>Gender:</label>
                     
                         <div className='radio-toolbar'>
-                            <input type="radio" name='gender'id='male'/><label for='male' className='male_label'><i className="fas fa-male"></i> Male</label>
-                            <input type='radio' name='gender' id='female'/><label for='female' className='female_label'><i type='radio' className="fas fa-female"></i> Female</label>
+                            <input type="radio" name='gender' value='male' onChange={this.handleChange}/><label for='male' className='male_label'><i className="fas fa-male"></i> Male</label>
+                            <input type='radio' name='gender' value='female' onChange={this.handleChange}/><label for='female' className='female_label'><i type='radio' className="fas fa-female"></i> Female</label>
                         </div>
                     </div>
                     <div>
                         <label className='form_labels' id='religion'>Religion: </label>
-                        <select id='religion'>
+                        <select id='religion' name='religion' onChange={this.handleChange}>
                             {
-                                religions.map(type => <option key={type}>{type}</option>)
+                                religions.map(type => <option value={type} key={type}>{type}</option>)
                             }
                         </select>
                     </div>
-
-
-
                     <div>
                         <label className='form_labels' id='ethnicity'>Ethnicity: </label>
-                        <select id='ethnicity'>
+                        <select id='ethnicity' name='ethnicity' onChange={this.handleChange}>
                             {
-                                ethnicity.map(type => <option key={type}>{type}</option>)
+                                races.map(type => <option value={type} key={type}>{type}</option>)
                             }
-                            
                         </select>
                     </div>
-
-                <div className='intro_extro'>Introvert<input className='slider'type="range" min="1" max="10"/>Extrovert</div>
-
+                <div className='intro_extro'>Introvert<input className='slider'type="range" min="1" max="10" name='intro_extro' value={intro_extro} onChange={this.handleChange}/>Extrovert</div>
                 <button onClick={this.flipInterestDisplay} className='interests_button' >What are your interests?</button>
                 {
                     display ? 
@@ -77,24 +107,27 @@ class About2 extends Component {
                         return (
                             <div>
                                 <label>{interest}</label>
-                                <input type="checkbox"/>     
+                                <input type="checkbox" onClick={this.handleCheck} name={interest} value="unchecked" />     
                             </div>
                         )
                     }) : 
                   <div>{null}</div>
                 }
-
                 <label id='description' className='form_labels'>Description:</label>
-                <textarea className='description' cols='30' rows='10' placeholder='Tell us about yourself'/>
-
+                <textarea className='description' cols='30' rows='10' placeholder='Tell us about yourself' name='description' value={description}/>
+                
                     <div className='form_one_button_container'>
-                            <button className='skip-button'>Skip</button>
-                            <button className='next-button'>Next</button>
+                        <Link to='/preferences'>
+                        <button className='skip-button'>Skip</button>
+                        <button className='next-button' onClick={this.handleSubmit}>Next</button>
+                        </Link>
                     </div>
                 </div>
             </div>
         )
     }
 }
-
-export default About2;
+function mapStateToProps(state) {
+    return state.form
+}
+export default connect(mapStateToProps, {addUserDetailsAndInterests})(About2);
