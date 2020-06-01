4c205d19bca0ffe7714e008dda59805b58fd0a74
Aaron Manwill <51334859+manwillaaron@users.noreply.github.com>
2020-06-01 14:52:01 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux'
 import store from './ducks/store'
-import {HashRouter as Router} from 'react-router-dom'
+import {BrowserRouter as Router} from 'react-router-dom'
 import './index.css';
 import App from './App';
 
